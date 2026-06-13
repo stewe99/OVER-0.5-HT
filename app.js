759d7c59130c93e04ca79b22c562cc06bef60031
stewe99 <stefano.sella34@yahoo.com>
2026-06-13 22:23:46 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const matches = [
+  {
+    score: 96,
+    home: "Malmo FF",
+    away: "Hammarby"
+  },
+  {
+    score: 95,
+    home: "Bodo/Glimt",
+    away: "Molde"
+  },
+  {
+    score: 94,
+    home: "PSV",
+    away: "Utrecht"
+  }
+];
+
+const container = document.getElementById("matches");
+
+matches.forEach(match => {
+  container.innerHTML += `
+    <div class="card">
+      <div class="score">${match.score}%</div>
+      <div class="match">
+        ${match.home}<br>
+        vs<br>
+        ${match.away}
+      </div>
+      <div class="badge">🔥 TOP PICK</div>
+    </div>
+  `;
+});
